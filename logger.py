70fc3b265feba589637e701938d21fc08d409566
Borys <borys@pop-os.localdomain>
2020-03-14 20:50:40 +0100

    Changed logger.py to write input from geiger counter to geigerResults.txt

--- logger.py
+++ logger.py
@@ -3,11 +3,16 @@ import time
 
 counter = geiger.GeigerCounter()
 
-while True:
-    counter.get_cpm()
-    counter.get_high_cpm()
-    counter.get_low_cpm()
-    counter.cpm_to_usv()
-    print(counter.usv_dose, "uSv/h ", counter.cpm, "CPM ", counter.high_cpm, "HICPM ", counter.low_cpm,
-          "LOCPM")
+f = open("geigerResults.txt", "a+")
 
+f.write("Geiger results. Date:"datetime.datetime.now()"\n")
+
+while True:
+    #counter.get_cpm()
+    #counter.get_high_cpm()
+    #counter.get_low_cpm()
+    #counter.cpm_to_usv()
+    #print(counter.usv_dose, "uSv/h ", counter.cpm, "CPM ", counter.high_cpm, "HICPM ", counter.low_cpm, "LOCPM")
+    f.write(counter.usv_dose, "uSv/h ", counter.cpm, "CPM ", counter.high_cpm, "HICPM ", counter.low_cpm, "LOCPM")
+
+f.close()
